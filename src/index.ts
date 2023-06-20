c2a23582228ef3533f0006a2b9477710d250861d
Sandeep K K <sandeepdotcode@gmail.com>
2023-06-20 16:45:03 +0530

    Implement landing page initial layout

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import './styles/reset.css';
+import './styles/style.css';
+
 const tsString = "I'm using TypeScript";
 
-console.log("Hello World! %s!", tsString);
+console.log('Hello World! %s!', tsString);
+
+fetch(
+  'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
